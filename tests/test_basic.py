51c4db5a21d87af506ed1903d9d2ad8c0b14bead
non <denjo2228@gmail.com>
2025-12-18 15:16:30 +0900

    テストファイルを新規作成し、pymcprotocol_fxseriesのインポートテストを追加

---
+++ tests/test_basic.py
@@ -0,0 +1,8 @@
+import pymcprotocol_fxseries
+
+def test_import():
+  """
+  If pymcprotocol_fxseries is not empty (None), pass.
+  (pymcprotocol_fxseriesが空（None）でなければ合格)
+  """
+  assert pymcprotocol_fxseries is not None
